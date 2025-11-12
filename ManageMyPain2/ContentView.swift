a3cabbd498d760d6e5b41d944feaf1fb501b3b4b
pourmaz <pourmaz@yahoo.com>
2025-11-12 11:19:15 +0100

    Initial Commit

---
+++ ManageMyPain2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ManageMyPain2
+//
+//  Created by Pourya Mazinani on 12/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ManageMyPain2/ManageMyPain2App.swift
@@ -0,0 +1,17 @@
+//
+//  ManageMyPain2App.swift
+//  ManageMyPain2
+//
+//  Created by Pourya Mazinani on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ManageMyPain2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
